5ad140aa242c54271d814970ff67d86256e29606
Shrijal Gupta <72612794+Shrijalexpo@users.noreply.github.com>
2022-02-20 00:53:18 +0530

    v1.1
    > Added Notification Sending Feature
    > Removed Bugs and Other Improvements

--- expo-Net.py
+++ expo-Net.py
@@ -1,6 +1,5 @@
-#!/bin/bash
-
-from plyer import notification
+import os
+from gi.repository import Notify, GdkPixbuf
 import speedtest
 import netifaces
 from datetime import datetime
@@ -8,106 +7,114 @@ import os
 import time as t
 import urllib.request
 
+
 def ip():
     gws = netifaces.gateways()
     ip = gws['default'][netifaces.AF_INET][0]
-    file = open('expo_Networking_logs.txt','a')
-    file.write("\n Gateway ip "+ip)
+    file = open('expo-Net-logs.txt', 'a')
+    file.write("\n Gateway ip " + ip)
     file.close()
     global host
-    host = "http://"+ ip + "/"
+    host = "http://" + ip + "/"
+
 
 def connect():
     try:
         ip()
-        urllib.request.urlopen(host) 
+        urllib.request.urlopen(host)
         return True
     except:
         return False
+
 
 def internet(host='https://fast.com/'):
     try:
-        urllib.request.urlopen(host) 
+        urllib.request.urlopen(host)
         return True
     except:
         return False
 
-def notify2():
-    title = "expo-Net"
-    notification.notify(title= title,
-                    message= notify,
-                    app_icon = None,
-                    timeout= 10,
-                    toast=False)
 
-global notify
-speed =0
+def notify():
+    Notify.init("expo-Net")
+    summary = "expo-Net"
+    notification = Notify.Notification.new(summary, msg)
+    #file_name = os.getcwd + "/icon.jpg"
+    #image = GdkPixbuf.Pixbuf.new_from_file(file_name)
+    #notification.set_icon_from_pixbuf(image)
+    #notification.set_image_from_pixbuf(image)
+    notification.set_urgency(2)
+    notification.show()
+
+
+global msg
+speed = 0
 e2 = 0
 lim = 0
 iA = 0
-file = open('expo_Networking_logs.txt','w')
+file = open('expo_Networking_logs.txt', 'w')
 file.write("")
 file.close()
 while True:
-    
+
     if connect() == False:
         now = datetime.now()
-        file = open('expo_Networking_logs.txt','a')
+        file = open('expo_Networking_logs.txt', 'a')
         file.write(now.strftime("\n %d/%m/%Y %H:%M:%S"))
         if internet() == True:
             file.write(" Error_1 !!")
             file.close()
             if e2 >= 5:
                 t.sleep(60)
-                
+
         else:
+            iA = 0
             file.write(" Not Connected     No Internet Access")
             file.close()
-        
+
             if lim >= 5:
-                file = open('expo_Networking_logs.txt','a')
+                file = open('expo_Networking_logs.txt', 'a')
                 file.write(now.strftime("\n %d/%m/%Y %H:%M:%S"))
                 file.write(" Error_2 !!")
                 file.close()
-                notify = "Error in Connecting...retrying in 60s"
-                notify2()
-            
+                msg = "Error in Connecting...retrying in 60s"
+                notify()
+
                 t.sleep(60)
             else:
                 os.system("sudo nmcli networking off")
                 t.sleep(1)
                 os.system("sudo nmcli networking on")
                 lim += 1
-                file = open('expo_Networking_logs.txt','a')
+                file = open('expo_Networking_logs.txt', 'a')
                 file.write(now.strftime("\n %d/%m/%Y %H:%M:%S"))
                 file.write(" Reconneting...")
                 file.close()
                 t.sleep(5)
-                
+
     else:
 
         lim = 0
         e2 = 0
         now = datetime.now()
-        file = open('expo_Networking_logs.txt','a')
+        file = open('expo_Networking_logs.txt', 'a')
         file.write(now.strftime("\n %d/%m/%Y %H:%M:%S"))
         if internet() == True:
             iA += 1
             file.write(" Connected     Internet Access")
             if iA == 1:
-                 st = speedtest.Speedtest()
-                 speed = ((round(st.download()/1048576) + round(st.download()/1048576))/2)
+                st = speedtest.Speedtest()
+                speed = ((round(st.download() / 1048576) + round(st.download() / 1048576)) / 2)
 
-                 file.write("\n Internet Speed "+str(speed)+"Mbits")
-                 if speed < 10:
-                    notify = "Your Internet Connection is slow"
-                    notify2()
-                
+                file.write("\n Internet Speed " + str(speed) + " Mbits")
+                if speed < 20:
+                    msg = "Your Internet Connection speed is slow"
+                    notify()
+
             file.close()
         else:
+            iA = 0
             file.write(" Connected     No Internet Access")
             file.close()
-    
-        t.sleep(5)
 
-        
+        t.sleep(5)
